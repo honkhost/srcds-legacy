1fd1470a9159ea300e149b0b132b5092847a1a8f
Edwin Pers <pers.edwin@gmail.com>
2021-06-06 17:06:34 -0400

    switch to dotenv for confs

--- srcds_runner.mjs
+++ srcds_runner.mjs
@@ -6,6 +6,13 @@
 // Imports
 import { default as path } from 'path';
 
+// We're using dotenv for now to load config from .env, but once we're containerized we'll just get them directly
+import { default as dotenv } from 'dotenv';
+
+// Get config from .env
+const config = dotenv.config();
+console.log(config);
+
 // Sub to redis update channel
 
 // Enter user namespace
@@ -13,8 +20,8 @@ import { default as path } from 'path';
 // Create overlayfs mounts
 
 // Start srcds
-const gameDir = path.join(basedir, ident, gameID);
+//const gameDir = path.join(basedir, ident, gameID);
 
-const srcdsCommandLine = `${gameDir}/srcds_run -game ${game} -usercon -strictportbind -ip ${ip} -port ${port} +clientport ${clientPort} +tv_port ${HLTVPort} +sv_setsteamaccount ${gslt} -tickrate ${tickrate} +map ${defaultmap} +servercfgfile ${servercfg} -maxplayers_override ${maxplayers} +game_type ${gametype} +game_mode ${gamemode} -authkey ${wsapikey} -nobreakpad`;
+//const srcdsCommandLine = `${gameDir}/srcds_run -game ${game} -usercon -strictportbind -ip ${ip} -port ${port} +clientport ${clientPort} +tv_port ${HLTVPort} +sv_setsteamaccount ${gslt} -tickrate ${tickrate} +map ${defaultmap} +servercfgfile ${servercfg} -maxplayers_override ${maxplayers} +game_type ${gametype} +game_mode ${gamemode} -authkey ${wsapikey} -nobreakpad`;
 
-console.log(srcdsCommandLine);
+//console.log(srcdsCommandLine);
