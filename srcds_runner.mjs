720bcb4ffc622d5faaaf8319329a8c3d6130fdfc
Edwin Pers <pers.edwin@gmail.com>
2021-06-06 23:08:03 -0400

    process config from process.env

--- srcds_runner.mjs
+++ srcds_runner.mjs
@@ -8,20 +8,67 @@ import { default as path } from 'path';
 
 // We're using dotenv for now to load config from .env, but once we're containerized we'll just get them directly
 import { default as dotenv } from 'dotenv';
+// Populate process.env from .env
+dotenv.config();
 
-// Get config from .env
-const config = dotenv.config();
+const config = {
+  basedir: process.env.SRCDS_BASEDIR,
+  ident: process.env.SRCDS_IDENT,
+  gameid: process.env.SRCDS_GAMEID,
+  game: process.env.SRCDS_GAME,
+  ip: process.env.SRCDS_IP,
+  port: process.env.SRCDS_PORT,
+  clientPort: process.env.SRCDS_CLIENTPORT,
+  hltvPort: process.env.SRCDS_HLTVPORT,
+  tickrate: process.env.SRCDS_TICKRATE,
+  maxPlayers: process.env.SRCDS_MAXPLAYERS,
+  startupMap: process.env.SRCDS_STARTUPMAP,
+  serverCfgFile: process.env.SRCDS_SERVERCFGFILE,
+  gameType: process.env.SRCDS_GAMETYPE,
+  gameMode: process.env.SRCDS_GAMEMODE,
+  gslt: process.env.SRCDS_GSLT,
+  wsapikey: process.env.SRCDS_WSAPIKEY,
+};
+
 console.log(config);
-
 // Sub to redis update channel
 
-// Enter user namespace
+// Build directories
+const lowerDir = path.join(config.basedir, 'basefiles', config.gameid);
+const upperDir = path.join(config.basedir, 'serverfiles', config.ident, 'persistent');
+const workDir = path.join(config.basedir, 'serverfiles', config.ident, '.work');
+const mountPoint = path.join(config.basedir, 'serverfiles', config.ident, '740');
 
 // Create overlayfs mounts
+const overlayMountCommandLine = `mount -t overlay ${config.ident} -o \
+lowerdir=${lowerDir},\
+upperdir=${upperDir},\
+workdir=${workDir} \
+${mountPoint}
+`;
+
+console.log(overlayMountCommandLine);
 
 // Start srcds
-//const gameDir = path.join(basedir, ident, gameID);
+const gameDir = path.join(config.basedir, 'serverfiles', config.ident, config.gameid);
 
-//const srcdsCommandLine = `${gameDir}/srcds_run -game ${game} -usercon -strictportbind -ip ${ip} -port ${port} +clientport ${clientPort} +tv_port ${HLTVPort} +sv_setsteamaccount ${gslt} -tickrate ${tickrate} +map ${defaultmap} +servercfgfile ${servercfg} -maxplayers_override ${maxplayers} +game_type ${gametype} +game_mode ${gamemode} -authkey ${wsapikey} -nobreakpad`;
+const srcdsCommandLine = `${gameDir}/srcds_run \
+-game ${config.game} \
+-usercon \
+-strictportbind \
+-ip ${config.ip} \
+-port ${config.port} \
++clientport ${config.clientPort} \
++tv_port ${config.hltvPort} \
++sv_setsteamaccount ${config.gslt} \
+-tickrate ${config.tickrate} \
++map ${config.startupMap} \
++servercfgfile ${config.serverCfgFile} \
+-maxplayers_override ${config.maxPlayers} \
++game_type ${config.gameType} \
++game_mode ${config.gameMode} \
+-authkey ${config.wsapikey} \
+-nobreakpad \
+`;
 
-//console.log(srcdsCommandLine);
+console.log(srcdsCommandLine);
