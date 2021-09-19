5d305628059e1af1e90073c9760a7df3280dba55
Edwin Pers <pers.edwin@gmail.com>
2021-09-19 18:34:04 +0000

    Alpine

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -exu
 cp src/* dist/
-docker build -t registry.honkhost.gg/honkhost/srcds/csgo:latest-dev .
-docker push registry.honkhost.gg/honkhost/srcds/csgo:latest-dev
+docker build -t registry.honkhost.gg/honkhost/csgo:latest-dev .
+docker push registry.honkhost.gg/honkhost/csgo:latest-dev
