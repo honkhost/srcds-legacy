d776af6040706b8146cf7173baeb60dc977880de
Edwin Pers <pers.edwin@gmail.com>
2021-10-16 10:57:23 -0400

    update build to switch to latest-dev tag automatically

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 set -exu
+
+echo "Switching upstream tag for steamcmd to latest-dev for dev build"
+sed -i 's/registry.honkhost.gg\/honkhost\/steamcmd:latest/registry.honkhost.gg\/honkhost\/steamcmd:latest-dev/g' Dockerfile
+
 docker build -t registry.honkhost.gg/honkhost/csgo:latest-dev .
 docker push registry.honkhost.gg/honkhost/csgo:latest-dev
+
+echo "Switching upstream tag for steamcmd back to latest for autobuild"
+sed -i 's/registry.honkhost.gg\/honkhost\/steamcmd:latest-dev/registry.honkhost.gg\/honkhost\/steamcmd:latest/g' Dockerfile
